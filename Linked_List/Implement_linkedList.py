06961a3a88ab49e20dd924d2dcd7915b51d4e19e
HarshaYeluru <harsha.yeluru@gmail.com>
2022-12-24 14:27:35 +0000

    LL add reverse

--- Linked_List/Implement_linkedList.py
+++ Linked_List/Implement_linkedList.py
@@ -9,24 +9,23 @@ class linkedList:
     self.length = 1
 
   def printLL(self):
-    curr = self.head
-    out = ""
-    while True:
-      out = out + str(curr.value) + " --> "
-      if curr.next == None:
-        break
-      curr = curr.next
-    out += str(None)
-    print("Linked List has %d nodes"%(self.length))
-    print("Linked List: %s"%(out))
+    curr_node = self.head
+    if self.head is None:
+        print("Empty Linked List!")
+    else:
+        while curr_node is not None:
+            print(curr_node.value, end=" ")
+            curr_node = curr_node.next
+    print()
   
   def append(self, value):
     curr = self.head
     newNode = Node(value)
-    while curr.next == None:
-      curr.next = newNode
-      curr.next.next = None
-      self.length +=1
+    while curr.next != None:
+      curr = curr.next
+    curr.next = newNode
+    curr.next.next = None
+    self.length +=1
 
   def prepend(self, value):
     newNode = Node(value)
@@ -64,18 +63,26 @@ class linkedList:
         return True
       curr = curr.next
     print("%s not in LL"%(value))
-    
 
-myLL = linkedList(10)
-myLL.printLL()
+  def reverse(self):
+    if self.length == 1:
+      return self.head
+    ptr1 = self.head
+    ptr2 = self.head.next
+    while ptr2 is not None:
+      temp = ptr2.next
+      ptr2.next = ptr1
+      ptr1 = ptr2
+      ptr2 = temp
+    self.head.next = None
+    self.head = ptr1
+    self.printLL()
+      
+
+myLL = linkedList(1)
+myLL.append(2)
+myLL.append(3)
+myLL.append(4)
 myLL.append(5)
 myLL.printLL()
-myLL.prepend(16)
-myLL.printLL()
-myLL.insert(25, 2)
-myLL.printLL()
-myLL.remove(2)
-myLL.printLL()
-myLL.insert(25, 3)
-myLL.printLL()
-myLL.search(35)
+myLL.reverse()
